1aaff145e0bf0569e3b3621b4b8dfbbad70146d8
7devanle <7devanle@gmail.com>
2023-10-24 11:18:54 +0100

    added my first test; that would be in jumpoGame 2

--- devanle/jumpGameii/JumpGameTest.java
+++ devanle/jumpGameii/JumpGameTest.java
@@ -1,4 +1,17 @@
 package devanle.jumpGameii;
 
+import org.junit.*;
+
+import static org.junit.Assert.assertEquals;
+
 public class JumpGameTest {
+    private static Solution solution;
+    @Test
+    public void testJump(){
+        int[] nums = {2, 3, 1, 1, 4};
+        int expectedResult = 2; //expected result from the method
+
+        int result = solution.jump(nums);
+        assertEquals(expectedResult, result);
+    }
 }
